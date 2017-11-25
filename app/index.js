027c0670390691e8de2d004112d7c2ae0ba55c88
flyromance <flyromance@fanlong.local>
2017-11-25 22:19:26 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -3,12 +3,11 @@ import { render } from 'react-dom'
 import { hashHistory } from 'react-router'
 import { Provider } from 'react-redux'
 
-import configureStore from './store/configureStore'
-
-// 导入样式
 import './static/style/common.less'
 
-import RouterMap from './routes/routes'
+import configureStore from './store/configureStore'
+
+import RouterMap from './routes'
 
 const store = configureStore()
 
---
+++ app/routes/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Router, Route, IndexRoute } from 'react-router'
+import propTypes from 'prop-types'
+
+import Wrapper from '../pages/Wrapper'
+import Home from '../pages/Home'
+import City from '../pages/City'
+import Search from '../pages/Search'
+import Detail from '../pages/Detail'
+import User from '../pages/User'
+import NotFound from '../pages/NotFound'
+
+class RouterMap extends Component {
+    render() {
+        return (
+            <Router history={this.props.history} >
+                <Route path='/' component={Wrapper}>
+                    <IndexRoute component={Home} />
+                    <Route path='home' component={Home} />
+                    <Route path='city' component={City} />
+                    <Route path='search/:type(/:keyword)' component={Search} />
+                    <Route path='detail/:id' component={Detail} />
+                    <Route path='user' component={User} />
+                    <Route path='*' component={NotFound} />
+                </Route>
+            </Router>
+        )
+    }
+}
+
+RouterMap.propTypes = {
+    history: propTypes.object
+}
+
+
+export default RouterMap
